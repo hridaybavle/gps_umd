a6dfc3f2f4f8205ab8d40953aab92252c76702c9
Hriday <hriday.bavle@gmail.com>
2015-11-02 19:01:37 +0100

    making changes to get the start point in zero

--- gps_common/src/utm_odometry_node.cpp
+++ gps_common/src/utm_odometry_node.cpp
@@ -9,12 +9,59 @@
 #include <sensor_msgs/NavSatFix.h>
 #include <gps_common/conversions.h>
 #include <nav_msgs/Odometry.h>
+#include <gps_common/position.h>
+
+
 
 using namespace gps_common;
-
+Position::Position3D odom_3d;
 static ros::Publisher odom_pub;
 std::string frame_id, child_frame_id;
 double rot_cov;
+
+bool GlobalToLocal(Position::Pose3D *current)
+{
+    static Position::Pose3D map_origin;
+    static bool first_pose_received = false;
+
+    ROS_INFO("Global data (%.3f, %.3f, %.3f) (%.3f, %.3f, %.3f)",
+    current->x, current->y, current->z,
+    current->roll, current->pitch, current->yaw);
+
+    bool initial_pose = !first_pose_received;
+
+    if(initial_pose)
+    {
+        map_origin   = *current;
+        map_origin.x = map_origin.x;              //rint(map_origin.x/100000.0) * 100000.0;
+        map_origin.y = map_origin.y;             //rint(map_origin.y/100000.0) * 100000.0;
+        map_origin.z = map_origin.z;
+
+        first_pose_received = true;
+
+        ROS_INFO("INITIAL data (%.3f, %.3f, %.3f), map origin (%.3f, %.3f, %.3f)",
+        current->x, current->y, current->z,
+        map_origin.x, map_origin.y, map_origin.z);
+
+    }
+
+    current->x -= map_origin.x;
+    current->y -= map_origin.y;
+    current->z -= map_origin.z;
+
+    ROS_INFO("Local data  (%.3f, %.3f, %.3f) (%.3f, %.3f, %.3f)",
+    current->x, current->y, current->z,
+    current->roll, current->pitch, current->yaw);
+
+    return initial_pose;
+}
+
+bool ConvertToLocal(Position::Pose3D *current)
+{
+
+    if(GlobalToLocal(current))
+            return false;
+}
 
 void callback(const sensor_msgs::NavSatFixConstPtr& fix) {
   if (fix->status.status == sensor_msgs::NavSatStatus::STATUS_NO_FIX) {
@@ -26,12 +73,26 @@ void callback(const sensor_msgs::NavSatFixConstPtr& fix) {
     return;
   }
 
-  double northing, easting;
+  double  northing_double , easting_double;
   std::string zone;
 
-  LLtoUTM(fix->latitude, fix->longitude, northing, easting, zone);
+  LLtoUTM(fix->latitude, fix->longitude, northing_double, easting_double, zone);
+  int easting  = easting_double;
+  int northing = northing_double;
+  int altitude = fix->altitude;
 
   if (odom_pub) {
+
+    odom_3d.pos.x = northing;
+    odom_3d.pos.y = easting;
+    odom_3d.pos.z = altitude;
+
+    odom_3d.pos.roll  = 0;
+    odom_3d.pos.pitch = 0;
+    odom_3d.pos.yaw   = 0;
+
+    ConvertToLocal(&odom_3d.pos);
+
     nav_msgs::Odometry odom;
     odom.header.stamp = fix->header.stamp;
 
@@ -42,10 +103,11 @@ void callback(const sensor_msgs::NavSatFixConstPtr& fix) {
 
     odom.child_frame_id = child_frame_id;
 
-    odom.pose.pose.position.x = easting;
-    odom.pose.pose.position.y = northing;
-    odom.pose.pose.position.z = fix->altitude;
-    
+    odom.pose.pose.position.x = odom_3d.pos.x;
+    odom.pose.pose.position.y = odom_3d.pos.y;
+    odom.pose.pose.position.z = odom_3d.pos.z;
+
+
     odom.pose.pose.orientation.x = 0;
     odom.pose.pose.orientation.y = 0;
     odom.pose.pose.orientation.z = 0;
@@ -73,6 +135,7 @@ void callback(const sensor_msgs::NavSatFixConstPtr& fix) {
     odom.pose.covariance = covariance;
 
     odom_pub.publish(odom);
+    
   }
 }
 
@@ -80,15 +143,21 @@ int main (int argc, char **argv) {
   ros::init(argc, argv, "utm_odometry_node");
   ros::NodeHandle node;
   ros::NodeHandle priv_node("~");
+  ros::Rate loop_rate(1);   
+
 
   priv_node.param<std::string>("frame_id", frame_id, "");
   priv_node.param<std::string>("child_frame_id", child_frame_id, "");
   priv_node.param<double>("rot_covariance", rot_cov, 99999.0);
 
-  odom_pub = node.advertise<nav_msgs::Odometry>("odom", 10);
+  odom_pub = node.advertise<nav_msgs::Odometry>("drone0/odom", 10);
 
   ros::Subscriber fix_sub = node.subscribe("fix", 10, callback);
-
+  
   ros::spin();
+  loop_rate.sleep();
+ 
+
+
 }
 
